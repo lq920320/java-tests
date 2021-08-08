af893a7d22c77e340e3fed48d2781caab2d10764
泽兔 <hunter321@dingtalk.com>
2021-08-08 16:23:58 +0800

    add: hutool randomStr test

--- src/test/java/other/StringUtilsEqualTest.java
+++ src/test/java/other/StringUtilsEqualTest.java
@@ -1,7 +1,13 @@
 package other;
 
+import cn.hutool.core.util.RandomUtil;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.BeanUtilsBean;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
+
+import java.util.Locale;
 
 /**
  * @author liuqian
@@ -17,5 +23,18 @@ public class StringUtilsEqualTest {
         // StringUtils.equals(CharSequence cs1, CharSequence cs2) 两个参数皆为 nullable
         System.out.println(StringUtils.equals(a, c));
         System.out.println(StringUtils.equals(b, c));
+    }
+
+    @Test
+    public void upperCaseTest() {
+        String a = "abcdefghijklmnopqrstuvwxyz";
+        System.out.println(a.toUpperCase(Locale.ROOT));
+    }
+
+    @Test
+    public void randomStr() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(RandomUtil.randomString("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 10));
+        }
     }
 }
